1a2bea078f833f3efd5901c8d97fe20fdf08f126
amyruyuyan <41204932+amyruyuyan@users.noreply.github.com>
2020-07-26 14:27:22 -0700

    💖

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,26 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	
+int uwu;
+JFrame frame;
+public static final int WIDTH = 500;
+public static final int HEIGHT = 800;
+	
+public static void main(String[] args) {
+	LeagueInvaders LeagueInvaders = new LeagueInvaders();
+	LeagueInvaders.setup();
 }
+
+void setup(){
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	frame.setVisible(true);
+	frame.setSize(WIDTH, HEIGHT);
+}
+
+public LeagueInvaders(){
+	frame = new JFrame();
+}
+}
+	
+
